44ce8b4ecd907ca84bfb41fe95dbb0c9430236b8
ImmORTxx <118477732+ImmORTxx@users.noreply.github.com>
2026-02-17 09:28:15 +0530

    Refactor PalindromeCheckerApp.java by removing comments
    Removed commented sections for clarity and streamlined the main method.

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -7,9 +7,7 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // =============================
-        // UC1 - Welcome Message
-        // =============================
+        
         System.out.println("==================================");
         System.out.println("     WELCOME TO PALINDROME APP    ");
         System.out.println("==================================");
@@ -17,24 +15,18 @@ public class PalindromeCheckerApp {
         System.out.println("Version          : " + APP_VERSION);
         System.out.println("----------------------------------");
 
-        // =============================
-        // UC2 - Hardcoded Palindrome
-        // =============================
+        
         String hardcodedWord = "madam";
         System.out.println("\nUC2 - Hardcoded Check:");
         checkAndPrint(hardcodedWord);
 
-        // =============================
-        // UC3 - User Input
-        // =============================
+        
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\nUC3 - Enter a string to check:");
         String userInput = scanner.nextLine();
 
-        // =============================
-        // UC4 & UC5 - Case Insensitive + Ignore Special Characters
-        // =============================
+        
         String cleanedInput = cleanString(userInput);
 
         System.out.println("\nProcessed String: " + cleanedInput);
@@ -53,9 +45,7 @@ public class PalindromeCheckerApp {
         System.out.println("\nThank you for using Palindrome Checker App!");
     }
 
-    // =============================
-    // Basic Reverse Method (UC2)
-    // =============================
+    =
     public static void checkAndPrint(String word) {
 
         String reversed = "";
@@ -71,19 +61,13 @@ public class PalindromeCheckerApp {
         }
     }
 
-    // =============================
-    // Clean String (UC4 & UC5)
-    // Remove spaces & special characters
-    // Convert to lowercase
-    // =============================
+   
     public static String cleanString(String input) {
         return input.replaceAll("[^a-zA-Z0-9]", "")
                 .toLowerCase();
     }
 
-    // =============================
-    // Two Pointer Approach (Optimized)
-    // =============================
+   
     public static boolean isPalindromeTwoPointer(String str) {
 
         int left = 0;
